f87ea45e5ec3d49cc0f09368c0cf4b3e63df0ad2
NotBen <benramirez805@gmail.com>
2024-09-02 15:06:12 -0700

    Add project files.

---
+++ Even or Odd/Program.cs
@@ -0,0 +1,31 @@
+﻿{
+    Console.WriteLine("Enter Number Here");
+
+    int num1 = int.Parse(Console.ReadLine());
+
+   
+
+
+
+
+
+    if (num1 % 2 == 0) 
+    {
+        
+        Console.WriteLine("Even");
+
+    }
+    else
+    {
+        Console.WriteLine("Odd");
+    }
+
+
+
+
+
+
+
+
+
+}
